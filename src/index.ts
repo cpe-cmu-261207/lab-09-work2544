5d64e90a6605c5ee906f735b83296b11cbd86859
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2021-09-25 00:40:21 +0700

    add

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ declare global {
 import express, { Request, Response, NextFunction } from 'express'
 import auth from 'basic-auth'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt, { hash } from 'bcrypt'
 
 const app = express()
 app.use(express.json())
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 }));
 
 type Role = 'admin' | 'customer'
-
+type UserorOther=User|any
 type User = {
   username: string
   password: string
@@ -35,34 +35,117 @@ const initialUsers: User[] = [
   },
 ]
 
-let users: User[] = [...initialUsers]
+var users: User[] = [...initialUsers]
 
 const SECRET = 'mysecret'
+type MyJwtPayload = {
+  username: string
+} & jwt.JwtPayload
 
 app.get('/user/login', async (req, res) => {
   const user = auth(req)
   if (!user) return res.status(404).json({ status: 'failed', message: 'Invalid username or password' })
-
   const username = user.name
   const password = user.pass
-})
+  const foundUser = users.find(x => x.username === username && bcrypt.compareSync(password, x.password))
+  console.log(users)
+  if (!foundUser) return res.status(404).json({ status: 'failed',"message" : "Invalid username or password" })
+  const token = jwt.sign({ username }, SECRET, { expiresIn: '10h' })
+  return res.json({ status: 'success', token })
+})//ok
 
-app.post('/user/regis', async (req, res) => {
+app.post('/user/regis', async (req, res) => {  
+  const user=req.body;
+  let ReqKey=Object.keys(req.body)
+  if ('username'==ReqKey[0]&&'password'==ReqKey[1]&&'role'==ReqKey[2]&&ReqKey.length===3)  
+  {
+    const username = user.username
+    const password=user.password
+    if(users.find(x=>x.username===username)!==undefined)
+    {
+      return res.status(400).json({status: 'failed', message: 'Username is already used'})
+    }
+    if(typeof(user.username)!=="string"&&
+       typeof(user.password)!=="string"&&
+       typeof(user.role)!=="string")
+       {
+        return res.status(400).json({status: 'failed', message: 'Invalid input'})
+       }
+       
+    if(user.role=="admin")
+    {
+      if(user.money!=null)
+      {
+        return res.status(400).json({status: 'failed', message: 'Invalid input'})
+      }
+      user.password=bcrypt.hashSync(password,10)
+      users.push(user)
+      return res.status(200).json({status: 'success', username:username})
+    }
+    else if(user.role=="customer")
+    {
+      if(user.money==null)
+      {
+        return res.status(400).json({status: 'failed', message: 'Invalid input'})
+      }
+      user.password=bcrypt.hashSync(password,10)
+      users.push(user)
+      return res.status(200).json({status: 'success', username:username})
+    }
+  }  
     return res.status(401).json({status: 'failed', message: 'Authentication failed'})
-    return res.status(400).json({status: 'failed', message: 'Username is already used'})
-    return res.status(400).json({status: 'failed', message: 'Invalid input'})
-})
-
-app.get('/money', async (req, res) => {
-    return res.status(401).json({status: 'failed', message: 'Authentication failed'})
+})//ok
+const checkToken = async (req: Request, res: Response, next: NextFunction) => {
+  const bearerHeader = req.headers['authorization']
+  if (bearerHeader) {
+    const splited = bearerHeader.split(' ')
+    const token = splited[1]
+    try {
+      const decoded = jwt.verify(token, SECRET) as MyJwtPayload
+      const username = decoded.username
+      req.username = username
+      next()
+    } catch {
+      return res.status(401).json({ status: 'failed'})
+    }
+  }
+  else
+    return res.status(401).json({ status: 'failed' })
+}
+app.get('/money',checkToken, async (req, res) => {
+  const foundUser = users.find(x => x.username === req.username) as User
+  if (!foundUser||foundUser.role=="admin")  return res.status(401).json({status: 'failed', message: 'Authentication failed'})
+  return res.json({
+    status: 'success',
+    username: foundUser.username,
+    money: foundUser.money
+  })  
 })
 
 app.put('/money/topup', (req, res) => {
-    return res.status(401).json({status: 'failed', message: 'Authentication failed'})
-    return res.status(400).json({status: 'failed', message: "Invalid input"})
+  let ReqKey=Object.keys(req.body)
+  if ('username'==ReqKey[0]&&'amount'==ReqKey[1]&&ReqKey.length===2)  
+  {
+  const foundUser = users.find(x => x.username === req.body.username) as User
+  if (!foundUser)  return res.status(401).json({status: 'failed', message: 'Authentication failed'})
+  const amount = req.body.amount
+  if(amount<0||foundUser.role=="admin")
+  return res.status(400).json({status: 'failed', message: "Invalid input"})
+  foundUser.money += amount
+  return res.json({
+    status: 'success',
+    username:foundUser.username,
+    money: foundUser.money
+  })
+}
+else
+{
+  return res.status(401).json({status: 'failed', message: 'Authentication failed'})
+}
 })
 
 app.delete('/reset', (req, res) => {
+  users=[initialUsers[0]]
   return res.json({status: 'success'})
 })
 
